14e9b27308fafa56b3a2dc05271e37b685edb525
ToledoEM <8144940+ToledoEM@users.noreply.github.com>
2026-03-13 22:26:23 +0000

    root user

--- bentopdf/run.sh
+++ bentopdf/run.sh
@@ -6,6 +6,4 @@ LOG_LEVEL=$(bashio::config 'log_level' 'info')
 
 bashio::log.info "Starting BentoPDF (log_level=${LOG_LEVEL})"
 
-mkdir -p /tmp/nginx/logs /tmp/nginx/tmp
-
-exec nginx -g "daemon off;"
+exec nginx
